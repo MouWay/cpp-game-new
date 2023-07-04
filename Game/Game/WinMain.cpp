273034c400aa6dd4358e2bb0a875070754d5f45a
Matvey <matvey.tanyushkin@gmail.com>
2023-07-04 03:55:29 +0300

    Initial commit

---
+++ Game/Game/WinMain.cpp
@@ -0,0 +1,24 @@
+#include "Window.h"
+
+int CALLBACK WinMain(
+    HINSTANCE hInstance,
+    HINSTANCE hPrevInstance,
+    LPSTR lpCmdLine,
+    int nCmdShow)
+{
+    Window wnd(800, 300, "Game");
+
+    MSG msg;
+    BOOL gResult;
+    while (gResult = GetMessage(&msg, nullptr, 0, 0) > 0) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    if (gResult == -1) {
+        return -1;
+    }
+    else {
+        return msg.wParam;
+    }
+}
